ff60dc9a590809675b01e259d335e4a0dad925a5
joaovpimenta <joaovpiment@gmail.com>
2023-06-22 11:42:04 -0300

    Initial commit

---
+++ src/test/java/br/com/joaovpimenta/api/ApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaovpimenta.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
